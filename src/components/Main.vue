0f12c093d15179a691a27e0d7cf519bf1d54cacf
Romain Demeure <demeureromain4@gmail.com>
2024-03-05 09:42:43 +0100

    test

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,17 @@
 <template lang="">
-    <div>
-        <p>Hello World</p>
-    </div>
+    <section class="home-section">
+
+    </section>
 </template>
+
+<style scoped>
+.home-section {
+    height: 2000px;
+    background: red;
+}
+</style>
 <script>
 export default {
     
 }
 </script>
-<style scoped>
-    
-</style>
